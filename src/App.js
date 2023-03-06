b8ffcfd8b228c61d3db5ed2a9f68f1c88aeea2fc
Claucio <claucio2@gmail.com>
2023-03-06 17:39:50 -0300

    primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        
+      </header> 
+
+      <body>
+          <div id="boarding-pass">
+          <h1>Cartão de embarque</h1>
+          <main id="ticket">
+            <section class="flight-info"></section>
+            <section class="passanger-info"></section>
+            <section class="boarding-info"></section>
+          </main>
+          <footer>Qualquer problema procure o balcão de atendimento da sua companhia aérea</footer>
+          </div>
+      </body>
     </div>
   );
 }
