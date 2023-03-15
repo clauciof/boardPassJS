7e9906a9097705919ae4909ce529f44fcaae2ca7
Claucio <claucio2@gmail.com>
2023-03-14 23:57:48 -0300

    formando o cartao de embarque, estrutura básica

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
       </header> 
 
       <body>
-          <div id="boarding-pass">
+        <div id="boarding-pass">
           <h1>Cartão de embarque</h1>
           <main id="ticket">
             <section class="flight-info">
@@ -73,8 +73,8 @@ function App() {
               <p><strong>Atenção:</strong> o portão fecha as 16:45</p>    
             </section>
           </main>
-          <footer>Qualquer problema procure o balcão de atendimento da sua companhia aérea</footer>
-          </div>
+          <footer id="footer">Qualquer problema procure o balcão de atendimento da sua companhia aérea</footer>
+        </div>
       </body>
     </div>
   );
